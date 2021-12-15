b6a00a7c83224fbe46dd6b6a3968f26024832640
André Escarião <andre.escariao1@gmail.com>
2021-12-15 10:40:11 -0300

    Update GPSARDUINO-Sketch.ino

--- GPSARDUINO-Sketch.ino
+++ GPSARDUINO-Sketch.ino
@@ -5,7 +5,7 @@ Projeto GPS e SD para Arduino:
 Código que faz comunicação serial com o módulo GPS e SD. Com a utilização de quatro bibliotecas <SoftwareSerial.h>, <TinyGPS.h>, <SD.h> e <SPI.h> a IDE recebe informações
 do módulo GPS - latitude, longitude - por meio de satélites e armazena no cartão SD.
 
-O projeto consiste em gravar as posições de locomoção de um dispositivo ou automóvel de uma localidade para outra.
+O projeto consiste em gravar as posições de locomoção de um dispositivo de uma localidade para outra.
 Assim, o sistema possuirá dois botões. Um será utilizado para iniciar a gravação das posições e o segundo, será utilizado para finalizar as gravações.
 Além disso, teremos um LED para sinalizar o funcionamento e gravação dos dados do GPS em tempo real.
 */
