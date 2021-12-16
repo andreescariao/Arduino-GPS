70d8c8f42439dc2b46eb1a1f6431b8bc7e7c5ae0
André Escarião <andre.escariao1@gmail.com>
2021-12-16 19:21:31 -0300

    Update GPSARDUINO-Sketch.ino

--- GPSARDUINO-Sketch.ino
+++ GPSARDUINO-Sketch.ino
@@ -1,12 +1,12 @@
 /*
-Desenvolvimento: André Escarião, Graduando em Engenharia Elétrica(UFCG), Processo Seletivo Parahyasas
+Desenvolvimento: André Escarião, Graduando em Engenharia Elétrica(UFCG)
 
 Projeto Arduino-GPS:
 
-O projeto consiste em gravar as posições de locomoção de um dispositivo de uma localidade para outra.
+O projeto grava dados referentes ao tempo e posições de locomoção de um dispositivo de uma localidade para outra.
 Assim, o sistema possuirá dois botões. Um será utilizado para iniciar a gravação das posições e o segundo, será utilizado para finalizar as gravações.
 Além disso, teremos um LED para sinalizar o funcionamento e gravação dos dados do GPS em tempo real.
-Dessa forma, a IDE recebe informações do módulo GPS - latitude, longitude, altitude, velocidade e sentido - por meio de satélites e armazena no Cartão Micro SD.
+Dessa forma, a IDE recebe informações do módulo GPS - latitude, longitude, data, hora, altitude, velocidade e sentido - por meio de satélites e armazena no Cartão Micro SD.
 */
 
 
